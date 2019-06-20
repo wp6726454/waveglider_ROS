708319a955d7436721034669f7ccedd0138512d0
=wpeve <wp6726454@sjtu.edu.cn>
2019-06-20 21:30:05 +0800

    lmureader

--- src/navigation/src/lmu_pub.py
+++ src/navigation/src/lmu_pub.py
@@ -2,17 +2,43 @@
 '''lmu ROS Node'''
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64
+import serial
+import sys
+
+try:
+    ser=serial.Serial('/dev/ttyACM0',9600)
+
+except Exception, e:
+    print 'open serial failed.'
+    exit(1)
+
+while True:
+    s = ser.readline()
+    am=str(s).strip().split(" ")
+    if len(am)<14:
+        continue
+    else:
+        a_x=am[2]
+        a_y=am[4]
+        a_z=am[6]
+        m_x=am[9]
+        m_y=am[11]
+        m_z=am[13]
+
+  
+
+
+
+
 
 def talker():
     '''lmu Publisher'''
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('/Course_real', Float64, queue_size=10)
     rospy.init_node('lmu', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        pub.publish(phi)
         rate.sleep()
 
 if __name__ == '__main__':
