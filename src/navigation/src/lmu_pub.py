1d6c22c5736e2e749c13bc2af2eccf84ff8bf0fc
=wpeve <wp6726454@sjtu.edu.cn>
2019-06-20 22:08:48 +0800

    lmureader update

--- src/navigation/src/lmu_pub.py
+++ src/navigation/src/lmu_pub.py
@@ -4,42 +4,45 @@
 import rospy
 from std_msgs.msg import Float64
 import serial
-import sys
-
-try:
-    ser=serial.Serial('/dev/ttyACM0',9600)
-
-except Exception, e:
-    print 'open serial failed.'
-    exit(1)
-
-while True:
-    s = ser.readline()
-    am=str(s).strip().split(" ")
-    if len(am)<14:
-        continue
-    else:
-        a_x=am[2]
-        a_y=am[4]
-        a_z=am[6]
-        m_x=am[9]
-        m_y=am[11]
-        m_z=am[13]
-
-  
-
-
-
-
+import numpy as np
+from math import sin, cos, atan
 
 def talker():
     '''lmu Publisher'''
     pub = rospy.Publisher('/Course_real', Float64, queue_size=10)
     rospy.init_node('lmu', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        pub.publish(phi)
-        rate.sleep()
+    try:
+        ser=serial.Serial('/dev/ttyACM0',9600)
+
+    except Exception:
+        print 'open serial failed.'
+        exit(1)
+
+    while True:
+        s = ser.readline()
+        am=str(s).strip().split(" ")
+        if len(am)<14:
+            continue
+        else:
+            a_x=float(am[2])
+            a_y=float(am[4])
+            a_z=float(am[6])
+            m_x=float(am[9])
+            m_y=float(am[11])
+            m_z=float(am[13])
+
+    
+        eta=atan(a_x/(np.sqrt(np.square(a_y)+np.square(a_z))))
+        thita=atan(a_y/(np.sqrt(np.square(a_x)+np.square(a_z))))
+        Hy=m_y*cos(thita)+m_x*sin(thita)*sin(eta)-m_z*cos(eta)*sin(thita)
+        Hx=m_x*cos(eta)+m_z*sin(eta)
+        phi=atan(Hy/Hx)
+
+
+        while not rospy.is_shutdown():
+            pub.publish(phi)
+            rate.sleep()
 
 if __name__ == '__main__':
     try:
